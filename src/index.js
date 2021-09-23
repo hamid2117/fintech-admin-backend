5be7a968bf614cc916d21929ff855fa11e3677b5
hamid2117 <saim008865@gmail.com>
2021-09-23 12:02:40 +0500

    This is my commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,5 @@ import app from './app.js'
 // const port = 5000
 const port = process.env.PORT || 5000
 app.listen(port, () => {
-  console.log(
-    `Server running in ${process.env.NODE_ENV} : http://localhost:${port}`
-  )
+  console.log(`Server running in  : ${port}`)
 })
