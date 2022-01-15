7ce4f5ff6d387c0dcb16d1d170bad87aa716d243
hamid2117 <saim008865@gmail.com>
2022-01-15 10:31:45 +0500

    port problem

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import app from './app.js'
 
 // const port = 5000
-const port =  5000
+const port =  process.env.PORT || 5000
 app.listen(port, () => {
   console.log(`Server running in  : ${port}`)
 })
